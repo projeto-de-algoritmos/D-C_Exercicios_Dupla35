8810f6e7fa95902e59b6d2dba9b73ab260c37d35
luiza <luizaesteves987@gmail.com>
2023-06-17 14:06:47 -0300

    alterando para manter janela ordenada

--- SlidingWindowMedian/mediana.js
+++ SlidingWindowMedian/mediana.js
@@ -5,26 +5,53 @@
  */
 
 function findMedian(nums) {
-  nums.sort((a, b) => a - b);
-  const n = nums.length;
+  const sortedNums = [...nums].sort((a, b) => a - b);
+  const n = sortedNums.length;
   if (n % 2 === 0) {
-    return (nums[n / 2] + nums[n / 2 - 1]) / 2;
+    return (sortedNums[n / 2] + sortedNums[n / 2 - 1]) / 2;
   } else {
-    return nums[Math.floor(n / 2)];
+    return sortedNums[Math.floor(n / 2)];
   }
 }
 
 function medianSlidingWindow(nums, k) {
   const result = [];
-  if (nums.length < k) {
-    return result;
-  }
+  const window = nums.slice(0, k);
+  const sortedWindow = [...window].sort((a, b) => a - b);
+  result.push(findMedian(sortedWindow));
 
-  for (let i = 0; i <= nums.length - k; i++) {
-    const window = nums.slice(i, i + k);
-    const median = findMedian(window);
-    result.push(median);
+  for (let i = k; i < nums.length; i++) {
+    const removeIdx = window.indexOf(nums[i - k]);
+    window.splice(removeIdx, 1);
+
+    const insertIdx = binarySearch(sortedWindow, nums[i]);
+    window.splice(insertIdx, 0, nums[i]);
+    sortedWindow.splice(insertIdx, 0, nums[i]);
+
+    if (window.length > k) {
+      const removeIdx = window.indexOf(nums[i - k + 1]);
+      window.splice(removeIdx, 1);
+      sortedWindow.splice(removeIdx, 1);
+    }
+
+    result.push(findMedian(sortedWindow));
   }
 
   return result;
+}
+
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length;
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  return left;
 }
