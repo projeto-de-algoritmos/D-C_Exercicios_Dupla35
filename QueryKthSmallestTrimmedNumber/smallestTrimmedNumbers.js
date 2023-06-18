f738d22bc6414b8aad3eb4474dc5b11ce295c0e6
luiza <luizaesteves987@gmail.com>
2023-06-18 20:58:20 -0300

    corrigindo problemas

--- QueryKthSmallestTrimmedNumber/smallestTrimmedNumbers.js
+++ QueryKthSmallestTrimmedNumber/smallestTrimmedNumbers.js
@@ -1,31 +1,31 @@
 function encontraMenor(nums, k, trim) {
-    const trimmedNums = nums.map(num => num.slice(-trim));
-    const indices = trimmedNums.map((_, index) => index).sort((a, b) => {
-      if (trimmedNums[a] === trimmedNums[b]) {
-        return b - a;
-      } else {
-        return trimmedNums[a].localeCompare(trimmedNums[b]);
-      }
-    });
-    return indices[k - 1];
-  }
-  
-  function smallestTrimmedNumbers(nums, queries) {
-    const resultado = [];
-  
-    for (let i = 0; i < queries.length; i++) {
-      const [k, trim] = queries[i];
-      const numerosOriginais = nums.length();
-  
-      for (let j = 0; j < nums.length; j++) {
-        nums[j] = nums[j].slice(-trim);
-      }
-  
-      const index = encontraMenor(nums, k, trim);
-      resultado.push(index);
-  
-      nums = numerosOriginais;
+  const trimmedNums = nums.map(num => num.slice(-trim));
+  const indices = trimmedNums.map((_, index) => index).sort((a, b) => {
+    if (trimmedNums[a] === trimmedNums[b]) {
+      return a - b;
+    } else {
+      return trimmedNums[a].localeCompare(trimmedNums[b]);
     }
-  
-    return resultado;
+  });
+  return indices[k - 1];
+}
+
+function smallestTrimmedNumbers(nums, queries) {
+  const resultado = [];
+
+  for (let i = 0; i < queries.length; i++) {
+    const [k, trim] = queries[i];
+    const numerosOriginais = nums.slice();
+
+    for (let j = 0; j < nums.length; j++) {
+      nums[j] = nums[j].slice(-trim);
+    }
+
+    const index = encontraMenor(nums, k, trim);
+    resultado.push(index);
+
+    nums = numerosOriginais;
   }
+
+  return resultado;
+}
